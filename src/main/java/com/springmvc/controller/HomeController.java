6e34b0183f3c2ed7712546f790bca15b4e3972d8
HHHYERI <retki_0534@gmail.com>
2024-10-05 00:10:19 +0900

    필요없는 선언 삭제

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -4,13 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.mail.internet.MimeMessage;
-
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
